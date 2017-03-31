fbc7ff092c5be1ad05a9646f5b61139298fd4ea0
Master.G <masterg@MGs-iMac.local>
2017-03-31 16:36:59 +0800

    update

---
+++ agent/vendor/github.com/master-g/omgo/utils/utils.go
@@ -0,0 +1,61 @@
+/*
+MIT License
+
+Copyright (c) 2017 Master.G
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+package utils
+
+import (
+	"bytes"
+	log "github.com/Sirupsen/logrus"
+	"github.com/davecgh/go-spew/spew"
+	"github.com/maruel/panicparse/stack"
+	"os"
+	"runtime/debug"
+)
+
+// PrintPanicStack prints panic stack info
+func PrintPanicStack(extras ...interface{}) {
+	if x := recover(); x != nil {
+		in := bytes.NewBufferString(string(debug.Stack()[:]))
+		goroutines, err := stack.ParseDump(in, os.Stdout)
+		if err != nil {
+			return
+		}
+
+		// Optional: Check for GOTRACEBACK being set, in particular if there is only
+		// one goroutine returned.
+
+		// Use a color palette based on ANSI code.
+		p := &stack.Palette{}
+		buckets := stack.SortBuckets(stack.Bucketize(goroutines, stack.AnyValue))
+		srcLen, pkgLen := stack.CalcLengths(buckets, false)
+		for _, bucket := range buckets {
+			log.Error(p.BucketHeader(&bucket, false, len(buckets) > 1))
+			log.Error("\n" + p.StackLines(&bucket.Signature, srcLen, pkgLen, false))
+		}
+
+		for k := range extras {
+			log.Errorf("EXTRAS#%v DATA:%v\n", k, spew.Sdump(extras[k]))
+		}
+	}
+}
