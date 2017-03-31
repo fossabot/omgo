b981b492ae6db8bbaffd224029b85acb0d4d6bea
Master.G <masterg@MGs-iMac.local>
2017-03-31 15:31:26 +0800

    update

--- main.go
+++ main.go
@@ -3,9 +3,14 @@ package main
 import (
 	"fmt"
 	"github.com/master-g/omgo/mt19937"
+	"github.com/master-g/omgo/utils"
+	"time"
 )
 
 func main() {
+	defer utils.PrintPanicStack(time.Now())
 	ctx := mt19937.NewContext(0)
 	fmt.Println(ctx.NextInt32())
+	a := make([]int, 3)
+	a[3] = 4
 }
