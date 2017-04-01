f7ee1f47b09c247a7936f68ada2456118f4d52be
Master.G <masterg@MGs-iMac.local>
2017-04-01 20:57:03 +0800

    update

--- main.go
+++ main.go
@@ -1,16 +1,11 @@
 package main
 
 import (
-	"fmt"
-	"github.com/master-g/omgo/mt19937"
+	"github.com/master-g/omgo/services"
 	"github.com/master-g/omgo/utils"
-	"os"
 )
 
 func main() {
-	defer utils.PrintPanicStack("wow" + string(os.PathSeparator) + "s")
-	ctx := mt19937.NewContext(0)
-	fmt.Println(ctx.NextInt32())
-	a := make([]int, 3)
-	a[3] = 4
+	defer utils.PrintPanicStack()
+	services.Init("backends", []string{"http://localhost:2379"}, []string{"snowflake"})
 }
