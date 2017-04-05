6fbff18cc959efe2227cd576f7fe5fe4c2f470c2
mg <dearunclexiao@qq.com>
2017-04-06 00:54:13 +0800

    update

---
+++ security/echd/ecdh.go
@@ -0,0 +1,13 @@
+package echd
+
+import (
+	"crypto"
+	"io"
+)
+
+type ECDH interface {
+	GenerateECKey(io.Reader) (crypto.PrivateKey, crypto.PublicKey, error)
+	Marshal(crypto.PublicKey) []byte
+	Unmarshal([]byte) (crypto.PublicKey, bool)
+	GenerateSharedSecret(crypto.PrivateKey, crypto.PublicKey) ([]byte, error)
+}
