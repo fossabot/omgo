2886ac7041b499a3bba97dca172f721d0e1af0e6
mg <dearunclexiao@qq.com>
2017-04-06 01:20:46 +0800

    update

---
+++ security/echd/curve25519.go
@@ -0,0 +1,62 @@
+package echd
+
+import (
+	"crypto"
+	"golang.org/x/crypto/curve25519"
+	"io"
+)
+
+type curve25519ECDH struct {
+	ECDH
+}
+
+func NewCurve25519ECDH() ECDH {
+	return &curve25519ECDH{}
+}
+
+// GenerateECKey will generate a private key and a public key for key exchange via Diffie-Hellman algorithm
+// see detail at https://tools.ietf.org/id/draft-josefsson-tls-curve25519-02.html
+func (e *curve25519ECDH) GenerateECKey(rand io.Reader) (crypto.PrivateKey, crypto.PublicKey, error) {
+	var pub, priv [32]byte
+	var err error
+
+	_, err = io.ReadFull(rand, priv[:])
+	if err != nil {
+		return nil, nil, err
+	}
+
+	priv[0] &= 248
+	priv[31] &= 127
+	priv[31] |= 64
+
+	curve25519.ScalarBaseMult(&pub, &priv)
+
+	return &priv, &pub, nil
+}
+
+func (e *curve25519ECDH) Marshal(p crypto.PublicKey) []byte {
+	pub := p.(*[32]byte)
+	return pub[:]
+}
+
+func (e *curve25519ECDH) Unmarshal(data []byte) (crypto.PublicKey, bool) {
+	var pub [32]byte
+	if len(data) != 32 {
+		return nil, false
+	}
+
+	copy(pub[:], data)
+	return &pub, true
+}
+
+func (e *curve25519ECDH) GenerateSharedSecret(privateKey crypto.PrivateKey, publicKey crypto.PublicKey) ([]byte, error) {
+	var priv, pub, secret *[32]byte
+
+	priv = privateKey.(*[32]byte)
+	pub = publicKey.(*[32]byte)
+	secret = new([32]byte)
+
+	curve25519.ScalarMult(secret, priv, pub)
+
+	return secret[:], nil
+}
