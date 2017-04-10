611565cc884bf65bae1185040bceacf397367b66
Master.G <masterg@MGs-iMac.local>
2017-04-10 20:30:58 +0800

    update

--- packet/packet.go
+++ packet/packet.go
@@ -1,1 +1,21 @@
 package packet
+
+// Packet wraps a byte slice as a binary buffer
+type Packet struct {
+	buf []byte
+	pos int
+}
+
+// Data returns packet's internal buffer
+func (p *Packet) Data() []byte {
+	return p.buf
+}
+
+// Len returns length of the packet's internal buffer
+func (p *Packet) Len() int {
+	return len(p.buf)
+}
+
+// ----------------------------------------------------------------------------
+// Read
+// ----------------------------------------------------------------------------
