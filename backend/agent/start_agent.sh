e9d0edfc3a3e8f1e9d89edd4d026298e23021909
Master.G <masterg@MGs-iMac.local>
2017-04-26 16:41:16 +0800

    update

--- backend/agent/start_agent.sh
+++ backend/agent/start_agent.sh
@@ -4,14 +4,14 @@
 # so other container can access this container via localhost:port
 # ps. --net=host is not working on macOS, see https://github.com/docker/for-mac/issues/68
 
-LOCALHOST=127.0.0.1
+HOST=127.0.0.1
 SID=agent1
 
 docker rm -f ${SID}
 docker build --no-cache --rm=true -t agent .
 docker run --name ${SID} -e SERVICE_ID=${SID} -e MACHINE_ID=1 -p 8888:8888 -d -P agent \
     -l :8888 \
-    -e http://127.0.0.1:2379
+    -e http://${HOST}:2379
 
 # register service
-curl -L -X PUT http://${LOCALHOST}:2379/v2/keys/backends/${SID} -d value=${LOCALHOST}:8888
+curl -L -X PUT http://${HOST}:2379/v2/keys/backends/agent/${SID} -d value=${HOST}:8888
