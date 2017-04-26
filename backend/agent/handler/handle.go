52d898ce4db2f85fa9752cd37aa33abcddacdd78
Master.G <masterg@MGs-iMac.local>
2017-04-26 12:28:57 +0800

    update

--- backend/agent/handler/handle.go
+++ backend/agent/handler/handle.go
@@ -6,29 +6,51 @@ import (
 	"crypto/rc4"
 	"fmt"
 	log "github.com/Sirupsen/logrus"
+	"github.com/golang/protobuf/proto"
 	pb "github.com/master-g/omgo/backend/agent/proto"
 	"github.com/master-g/omgo/backend/agent/types"
 	"github.com/master-g/omgo/net/packet"
+	"github.com/master-g/omgo/proto/pb"
 	"github.com/master-g/omgo/security/ecdh"
 	"github.com/master-g/omgo/services"
 	"google.golang.org/grpc/metadata"
 	"io"
-    "github.com/master-g/omgo/proto/pb"
+	"time"
 )
+
+func errorRsp(cmd proto_common.Cmd, msg proto.Message) {
+
+}
+
+func genRspHeader() *proto_common.RspHeader {
+	header := &proto_common.RspHeader{
+		Status:    proto_common.ResultCode_RESULT_OK,
+		Timestamp: uint64(time.Now().Unix()),
+	}
+
+	return header
+}
 
 func ProcHeartBeatReq(session *types.Session, reader *packet.RawPacket) []byte {
 	p := packet.NewRawPacket()
-    p.WriteS32(int32(proto_common.Cmd_HEART_BEAT_RSP))
+	p.WriteS32(int32(proto_common.Cmd_HEART_BEAT_RSP))
 	return p.Data()
 }
 
 func ProcGetSeedReq(session *types.Session, reader *packet.RawPacket) []byte {
-	tbl, _ := PacketReadSeedInfo(reader)
+	rsp := &proto_common.S2CGetSeedRsp{Header: genRspHeader()}
+
+	req := &proto_common.C2SGetSeedReq{}
+	if err := proto.Unmarshal(reader.Data(), req); err != nil {
+		log.Errorf("invalid protobuf :%v", err)
+		rsp.Header.Status = proto_common.ResultCode_RESULT_INTERNAL_ERROR
+	}
+
 	curve := ecdh.NewCurve25519ECDH()
 	x1, e1 := curve.GenerateECKeyBuf(rand.Reader)
-	key1 := curve.GenerateSharedSecretBuf(x1, tbl.ClientSendSeed)
+	key1 := curve.GenerateSharedSecretBuf(x1, req.GetSendSeed())
 	x2, e2 := curve.GenerateECKeyBuf(rand.Reader)
-	key2 := curve.GenerateSharedSecretBuf(x2, tbl.ClientRecvSeed)
+	key2 := curve.GenerateSharedSecretBuf(x2, req.GetRecvSeed())
 
 	encoder, err := rc4.NewCipher([]byte(fmt.Sprintf("%v%v", Salt, key2)))
 	if err != nil {
@@ -44,10 +66,18 @@ func ProcGetSeedReq(session *types.Session, reader *packet.RawPacket) []byte {
 	session.Decoder = decoder
 	session.SetFlagKeyExchanged()
 
+	rsp.SendSeed = e1
+	rsp.RecvSeed = e2
+	rspBytes, err := proto.Marshal(rsp)
+	if err != nil {
+		log.Errorf("failed to marshal response, error:%v", err)
+		return nil
+	}
+
+Response:
 	p := packet.NewRawPacket()
-	p.WriteS16(Code["get_seed_rsp"])
-	p.WriteBytes(e1)
-	p.WriteBytes(e2)
+	p.WriteS32(int32(proto_common.Cmd_GET_SEED_RSP))
+	p.WriteBytes(rspBytes)
 
 	return p.Data()
 }
@@ -90,12 +120,8 @@ func ProcUserLoginReq(session *types.Session, reader *packet.RawPacket) []byte {
 	}
 	go fetcherTask(session)
 	p := packet.NewRawPacket()
-	p.WriteS16(Code["user_login_succeed_rsp"])
+	p.WriteS32(int32(proto_common.Cmd_LOGIN_RSP))
 	p.WriteU32(session.UserID)
 
 	return p.Data()
-}
-
-func ProcPingReq(session *types.Session, reader *packet.RawPacket) []byte {
-	return nil
 }
