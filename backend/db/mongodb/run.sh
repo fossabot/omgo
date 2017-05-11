ddc0236645455990ffbf4113a1f545a1f7425a35
Master.G <masterg@MGs-iMac.local>
2017-05-11 15:48:43 +0800

    update

--- backend/db/mongodb/run.sh
+++ backend/db/mongodb/run.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
+# https://hub.docker.com/_/mongo/
+
 SID=mongodb1
+NETHOST=--net=host
+
+case "$(uname -s)" in
+   Darwin)
+     IPADDR=$(ifconfig en0 | grep "inet " | cut -d " " -f2)
+     NETHOST=''
+     ;;
+esac
 
 docker rm -f ${SID}
 
-docker run -d --name ${SID} \
+docker run -d --name ${SID} ${NETHOST} \
   -p 37017:27017 \
   -v ${PWD}/mongod.conf:/etc/mongod.conf \
   -v ${PWD}/db:/data/db \
