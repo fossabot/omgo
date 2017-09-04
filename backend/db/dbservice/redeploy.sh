41071d9e5bfca42850f58379558ead7bf73800c1
mg <master-g@users.noreply.github.com>
2017-09-04 20:18:11 +0800

    update

--- backend/db/dbservice/redeploy.sh
+++ backend/db/dbservice/redeploy.sh
@@ -8,8 +8,8 @@ export VERTX_CMD="run"
 mvn compile dependency:copy-dependencies
 java \
   -cp  $(echo target/dependency/*.jar | tr ' ' ':'):"target/classes" \
-  $LAUNCHER $VERTX_CMD $VERTICLE \
-  --redeploy="src/main/**/*" --on-redeploy="$CMD" \
-  --launcher-class=$LAUNCHER \
+  ${LAUNCHER} ${VERTX_CMD} ${VERTICLE} \
+  --redeploy="src/main/**/*" --on-redeploy="${CMD}" \
+  --launcher-class=${LAUNCHER} \
   -conf src/main/resources/config.json \
   $@
--- backend/db/dbservice/run.sh
+++ backend/db/dbservice/run.sh
@@ -26,6 +26,3 @@ docker build --no-cache --rm=true -t ${SERVICE_NAME} .
 docker run --rm -d ${NETHOST} -p ${PORT}:${PORT} \
     --name ${SID} \
     ${SERVICE_NAME} \
-
-# register service
-# curl -q -L -X PUT http://${LOCALHOST}:2379/v2/keys/backends/${SERVICE_NAME}/${SID} -d value=${IPADDR}:${PORT}
