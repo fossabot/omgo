55480d2062743ad83f3d3b57e49c3bc83e3941c8
mg <master-g@users.noreply.github.com>
2017-10-26 18:29:32 +0800

    update

---
+++ backend/agent/service.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	pb "github.com/master-g/omgo/proto/grpc/agent"
+	"golang.org/x/net/context"
+)
+
+type server struct{}
+
+// KickUser from agent server
+func (s *server) KickUser(ctx context.Context, userEntry *pb.Agent_UserEntry) (*pb.Agent_Result, error) {
+	return nil, nil
+}
